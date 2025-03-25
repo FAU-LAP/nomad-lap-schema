3aa0f1a11a6a312d971b79bb9075acdabbdca9bc
Johannes Lehmeyer <johannes.lehmeyer@fau.de>
2025-03-25 08:36:08 +0100

    tried adding some

--- src/nomad_lap_schema/schema_packages/schema_package.py
+++ src/nomad_lap_schema/schema_packages/schema_package.py
@@ -1,20 +1,20 @@
-# from typing import (
-#     TYPE_CHECKING,
-# )
+from typing import (
+    TYPE_CHECKING,
+)
 
-# if TYPE_CHECKING:
-#     from nomad.datamodel.datamodel import (
-#         EntryArchive,
-#     )
-#     from structlog.stdlib import (
-#         BoundLogger,
-#     )
+if TYPE_CHECKING:
+    from nomad.datamodel.datamodel import (
+        EntryArchive,
+    )
+    from structlog.stdlib import (
+        BoundLogger,
+    )
 
-# from nomad.datamodel.data import Schema
+from nomad.datamodel.data import Schema
 from nomad.datamodel.metainfo.annotations import ELNAnnotation
 from nomad.config import config
 from nomad.datamodel.metainfo.eln import ELNExperiment, ELNSample
-from nomad.metainfo import Quantity, SchemaPackage, SectionProxy
+from nomad.metainfo import Quantity, SchemaPackage, SectionProxy, Section
 
 configuration = config.get_plugin_entry_point(
     "nomad_lap_schema.schema_packages:schema_package_entry_point"
@@ -24,10 +24,30 @@ m_package = SchemaPackage()
 
 
 class Sample_LAP(ELNSample):
+    m_def = Section()
     process_of_origin = Quantity(
-        type=SectionProxy("Experiment_LAP"),
+        type=EntryArchive,
         description="The process of origin of the sample.",
         a_eln=ELNAnnotation(component="ReferenceEditQuantity"),
+    )
+
+
+class Wafer_LAP(Sample_LAP):
+    wafer_number = Quantity(
+        type=str,
+        description="Wafer Number provided by the manufacturer",
+        a_eln=ELNAnnotation(component="StringEditQuantity"),
+    )
+    manufacturer = Quantity(
+        type=str,
+        description="Manufacturer of the wafer",
+        a_eln=ELNAnnotation(component="StringEditQuantity"),
+    )
+    thickness = Quantity(
+        type=float,
+        unit="um",
+        description="Thickness of the wafer",
+        a_eln=ELNAnnotation(component="NumberEditQuantity"),
     )
 
 
