82bc6644238d828854d2bcc4dd7f3ea99ddd4f7a
Tanmay2028 <tanmay.kulkarni@fau.de>
2025-12-06 12:12:19 +0100

    ruff fix

--- src/nomad_lap_schema/schema_packages/__init__.py
+++ src/nomad_lap_schema/schema_packages/__init__.py
@@ -1,13 +1,12 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
 from pydantic import Field
+from nomad_lap_schema.schema_packages.schema_package import m_package
 
 
 class LAPSchemaPackageEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
-        from nomad_lap_schema.schema_packages.schema_package import m_package
-
         return m_package
 
 
